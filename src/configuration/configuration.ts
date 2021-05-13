fd00e6082bb599c36358f30968ff9848b8dfb14a
Nikolai Nikiforov <nikolai.nikiforov@deliveryhero.com>
2021-05-13 14:58:00 +0200

    Add configuration validator

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,3 +1,5 @@
+import {Value, values as v} from 'validate-value';
+
 export interface IApplicationConfiguration {
   http_server: IHttpServer;
 }
@@ -6,6 +8,27 @@ export interface IHttpServer {
   port: number;
 }
 
+const configurationValidator = new Value({
+  type: v.object,
+  properties: {
+    http_server: {
+      type: v.object,
+      properties: {
+        port: {
+          type: v.number,
+        },
+      },
+      required: ['port'],
+      additionalProperties: false,
+    },
+  },
+  required: ['http_server'],
+  additionalProperties: false,
+});
+
 // eslint-disable-next-line node/no-unpublished-require
 const configuration: IApplicationConfiguration = require('../../../data/application/default_configuration.json');
+
+// throws when validation fails
+configurationValidator.validate(configuration);
 export default configuration;
